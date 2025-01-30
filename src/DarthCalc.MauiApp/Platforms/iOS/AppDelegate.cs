6cc82a40f70965e9b9ad7780a0f9e911bc537265
DarthMahia <cplbimixte@gmail.com>
2025-01-30 11:59:09 +0100

    Create MAUI App

---
+++ src/DarthCalc.MauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DarthCalcMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
